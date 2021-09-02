f917a4d420e126ce9d342f0b07ea22c4a03162fa
INNOVA\muskaplan <muskaplan@innova.com.tr>
2021-09-02 11:52:59 +0300

    creational - singleton lazy loading & double checked locking

--- src/singleton/Singleton.java
+++ src/singleton/Singleton.java
@@ -2,13 +2,21 @@ package singleton;
 
 public class Singleton {
 
-    private static Singleton singleton = new Singleton();
+    private static Singleton singleton;
 
     private Singleton() {
-
     }
 
     public static Singleton getSingleton() {
+
+        if (singleton == null) { // double checked locking
+            synchronized (Singleton.class) { // thread safe
+                if (singleton == null) { // lazy loading
+                    singleton = new Singleton();
+                }
+            }
+        }
+
         return singleton;
     }
 }
